176b3e8e0549b862db3746443eb67fb740b6df95
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-11 20:16:46 +1000

    Split spawner to separate configuration.

--- hub-v1/jupyterhub_config.py
+++ hub-v1/jupyterhub_config.py
@@ -1,18 +1,1 @@
-from jupyterhub.spawner import LocalProcessSpawner
-
-class SingleUserLocalProcessSpawner(LocalProcessSpawner):
-
-    def user_env(self, env):
-        env['USER'] = 'eduk8s'
-        env['HOME'] = '/home/eduk8s'
-        env['SHELL'] = '/bin/bash'
-        return env
-
-    def make_preexec_fn(self, name):
-        def preexec():
-            pass
-        return preexec
-
-c.JupyterHub.spawner_class = SingleUserLocalProcessSpawner
-
 c.JupyterHub.authenticator_class = 'tmpauthenticator.TmpAuthenticator'
