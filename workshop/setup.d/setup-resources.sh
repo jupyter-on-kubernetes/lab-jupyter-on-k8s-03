d3ad53486d2cb0ac21b72faa8dcb6fd67d5c2455
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-12 16:53:02 +1000

    Add image pull secret.

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+envsubst < hub-v3/values.yaml.in > hub-v3/values.yaml
 envsubst < hub-v3/ingress.yaml.in > hub-v3/ingress.yaml
 envsubst < hub-v3/sources.yaml.in > hub-v3/sources.yaml
 envsubst < hub-v3/imagedestinations.yaml.in > hub-v3/imagedestinations.yaml
