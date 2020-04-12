60d40796f696bc39c74a535a0b8fb54047c1bbff
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-12 15:33:41 +1000

    Setup image destination for build.

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -2,3 +2,4 @@
 
 envsubst < hub-v3/ingress.yaml.in > hub-v3/ingress.yaml
 envsubst < hub-v3/sources.yaml.in > hub-v3/sources.yaml
+envsubst < hub-v3/imagedestinations.yaml.in > hub-v3/imagedestinations.yaml
