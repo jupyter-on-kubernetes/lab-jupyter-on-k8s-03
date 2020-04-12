3a837982a501ba1aa95712c64dbf4a815ea564b3
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-12 20:24:58 +1000

    Add kubespawner.

--- hub-v3/jupyterhub_config.py
+++ hub-v3/jupyterhub_config.py
@@ -1,3 +1,3 @@
 c.JupyterHub.authenticator_class = "tmpauthenticator.TmpAuthenticator"
 
-c.JupyterHub.spawner_class = "jupyterhub.spawner.SimpleLocalProcessSpawner"
+c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
