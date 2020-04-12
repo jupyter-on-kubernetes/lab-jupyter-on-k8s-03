0b26d285eafd7d1ad7a0a4ef2dc93627ab3c35be
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-12 20:40:09 +1000

    Change around listener ports.

--- hub-v3/jupyterhub_config.py
+++ hub-v3/jupyterhub_config.py
@@ -5,3 +5,10 @@ c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
 c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.hub_port = 8081
 c.JupyterHub.hub_connect_ip = "jupyterhub"
+
+c.ConfigurableHTTPProxy.api_url = 'http://127.0.0.1:8082'
+
+c.Spawner.start_timeout = 120
+c.Spawner.http_timeout = 60
+
+c.KubeSpawner.port = 8080
