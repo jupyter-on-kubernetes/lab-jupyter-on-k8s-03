d78fc1d8347f61b328d472147fa773c31e27d0fe
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-12 20:30:34 +1000

    Expose hub outside of container.

--- hub-v3/jupyterhub_config.py
+++ hub-v3/jupyterhub_config.py
@@ -1,3 +1,7 @@
 c.JupyterHub.authenticator_class = "tmpauthenticator.TmpAuthenticator"
 
 c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
+
+c.JupyterHub.hub_ip = "0.0.0.0"
+c.JupyterHub.hub_port = 8081
+c.JupyterHub.hub_connect_ip = "jupyterhub"
