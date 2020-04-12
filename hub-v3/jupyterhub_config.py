e4667149203300961210e713fb79118ec56665bd
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-12 20:48:38 +1000

    Try port 8080.

--- hub-v3/jupyterhub_config.py
+++ hub-v3/jupyterhub_config.py
@@ -2,6 +2,7 @@ c.JupyterHub.authenticator_class = "tmpauthenticator.TmpAuthenticator"
 
 c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
 
+c.JupyterHub.port = 8080
 c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.hub_port = 8081
 c.JupyterHub.hub_connect_ip = "jupyterhub"
@@ -10,5 +11,3 @@ c.ConfigurableHTTPProxy.api_url = 'http://127.0.0.1:8082'
 
 c.Spawner.start_timeout = 120
 c.Spawner.http_timeout = 60
-
-c.KubeSpawner.port = 8080
