a982a3d48d8d78f5b65b82e2be543057060c043a
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-04-12 13:21:09 +1000

    Drop incomplete config.

--- hub-v3/jupyterhub_config.py
+++ hub-v3/jupyterhub_config.py
@@ -1,9 +1,3 @@
-import os
-
 c.JupyterHub.authenticator_class = "tmpauthenticator.TmpAuthenticator"
 
 c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
-
-c.KubeSpawner.namespace = os.environ.get("SESSION_NAMESPACE")
-c.JupyterHub.hub_ip = 
-c.JupyterHub.hub_connect_ip =
