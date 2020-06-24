80f6fe21d864965c14eaaa64d750f808ace4d796
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-06-24 16:30:31 +1000

    Exercises were moved to a sub directory.

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-envsubst < hub-v3/values.yaml.in > hub-v3/values.yaml
-envsubst < hub-v3/deployment.yaml.in > hub-v3/deployment.yaml
-envsubst < hub-v3/ingress.yaml.in > hub-v3/ingress.yaml
+envsubst < exercises/hub-v3/values.yaml.in > exercises/hub-v3/values.yaml
+envsubst < exercises/hub-v3/deployment.yaml.in > exercises/hub-v3/deployment.yaml
+envsubst < exercises/hub-v3/ingress.yaml.in > exercises/hub-v3/ingress.yaml
