4d62d8e323574bd6372292f7736be959a71f1178
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-06-25 14:56:20 +1000

    Adjust for running KubeSpawner in same workshop pod.

--- exercises/jupyterhub-v3/jupyterhub_config.py
+++ exercises/jupyterhub-v3/jupyterhub_config.py
@@ -1,17 +1,26 @@
+import os
+
+ingress_protocol = os.environ["INGRESS_PROTOCOL"]
+ingress_domain = os.environ["INGRESS_DOMAIN"]
+workshop_namespace = os.environ["WORKSHOP_NAMESPACE"]
+session_namespace = os.environ["SESSION_NAMESPACE"]
+
 c.JupyterHub.authenticator_class = "tmpauthenticator.TmpAuthenticator"
 
 c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
 
-c.JupyterHub.port = 8080
+c.JupyterHub.port = 8000
 c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.hub_port = 8081
-c.JupyterHub.hub_connect_ip = "jupyterhub"
-
-c.ConfigurableHTTPProxy.api_url = 'http://127.0.0.1:8082'
+c.JupyterHub.hub_connect_ip = f"{session_namespace}.{workshop_namespace}.svc.cluster.local"
+c.ConfigurableHTTPProxy.api_url = f"http://{c.JupyterHub.hub_connect_ip}:8082"
 
 c.Spawner.start_timeout = 120
 c.Spawner.http_timeout = 60
 
+c.KubeSpawner.namespace = session_namespace
+
+"""
 import wrapt
 
 @wrapt.patch_function_wrapper('jupyterhub.proxy', 'ConfigurableHTTPProxy.add_route')
@@ -28,18 +37,4 @@ def _wrapper_add_route(wrapped, instance, args, kwargs):
         target = target.replace(old, new)
 
     return wrapped(routespec, target, data, *_args, **_kwargs)
-
-@wrapt.patch_function_wrapper('jupyterhub.spawner', 'LocalProcessSpawner.get_env')
-def _wrapper_get_env(wrapped, instance, args, kwargs):
-    env = wrapped(*args, **kwargs)
-
-    target = env.get('JUPYTERHUB_API_URL')
-
-    old = 'http://%s:%s' % (c.JupyterHub.hub_connect_ip, c.JupyterHub.hub_port)
-    new = 'http://127.0.0.1:%s' % c.JupyterHub.hub_port
-
-    if target and target.startswith(old):
-        target = target.replace(old, new)
-        env['JUPYTERHUB_API_URL'] = target
-
-    return env
+"""
