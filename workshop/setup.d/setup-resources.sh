002d64ea1801e524c50240dbc6c76045563672c2
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-06-26 14:01:00 +1000

    Fix up generation of skaffold file.

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+envsubst < exercises/jupyterhub-v4/skaffold.yaml.in > exercises/jupyterhub-v4/skaffold.yaml
+
 envsubst < exercises/jupyterhub-v4/resources/values.yaml.in > exercises/jupyterhub-v4/resources/values.yaml
 envsubst < exercises/jupyterhub-v4/resources/deployment.yaml.in > exercises/jupyterhub-v4/resources/deployment.yaml
 envsubst < exercises/jupyterhub-v4/resources/ingress.yaml.in > exercises/jupyterhub-v4/resources/ingress.yaml
