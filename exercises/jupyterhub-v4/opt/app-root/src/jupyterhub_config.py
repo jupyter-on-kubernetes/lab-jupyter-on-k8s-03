d1060f41609ffd7789cfffe0080179e163775bcc
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-06-26 14:58:49 +1000

    Fix configuration, commands and add missing system packages.

--- exercises/jupyterhub-v4/opt/app-root/src/jupyterhub_config.py
+++ exercises/jupyterhub-v4/opt/app-root/src/jupyterhub_config.py
@@ -1,25 +1,11 @@
-import os
-
-# Since running in a workshop environment, will need its configuration
-# in setting up the JupyterHub configuration.
-
-ingress_protocol = os.environ["INGRESS_PROTOCOL"]
-ingress_domain = os.environ["INGRESS_DOMAIN"]
-workshop_namespace = os.environ["WORKSHOP_NAMESPACE"]
-session_namespace = os.environ["SESSION_NAMESPACE"]
-
 # Set up authentication to allow anonymous user access.
 
 c.JupyterHub.authenticator_class = "tmpauthenticator.TmpAuthenticator"
 
-# Enable the use of the spawner for Kubernetes and set the target namespace
-# for creating the Jupyter notebook instances to be the session namespace 
-# of the workshop environment. Also specify the Jupyter notebook container
-# image to be used.
+# Enable the use of the spawner for Kubernetes and specify the Jupyter
+# notebook container image to be used.
 
 c.JupyterHub.spawner_class = "kubespawner.KubeSpawner"
-
-c.KubeSpawner.namespace = session_namespace
 
 c.KubeSpawner.image = "jupyter/minimal-notebook:latest"
 
@@ -29,7 +15,7 @@ c.KubeSpawner.image = "jupyter/minimal-notebook:latest"
 c.JupyterHub.port = 8000
 c.JupyterHub.hub_ip = "0.0.0.0"
 c.JupyterHub.hub_port = 8001
-c.JupyterHub.hub_connect_ip = f"{session_namespace}-jupyterhub.{workshop_namespace}.svc.cluster.local"
+c.JupyterHub.hub_connect_ip = "jupyterhub"
 c.ConfigurableHTTPProxy.api_url = f"http://127.0.0.1:8002"
 
 # Increase startup timeouts since Jupyter notebook instances are slower
